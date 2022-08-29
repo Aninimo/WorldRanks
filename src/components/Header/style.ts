97d5f4f2ec0462e6bee6128b4e23137920c5ce14
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2022-08-29 20:17:15 -0300

    Create style.ts

---
+++ src/components/Header/style.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const HeaderContainer = styled.header`
+  display: flex;
+  justify-content: space-between;
+  
+  button{
+    background: none;
+    border: none;
+    color: var(--text-color);
+    font-size: 2rem;
+  }
+`
