d11d9cbb33a3611adfc1e1d0083479e580113930
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2024-06-02 15:15:50 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,47 +1,56 @@
 import { useState } from 'react'
 import Link from 'next/link'
 import { Header } from '../components/Header'
-import { Input } from '../components/Input'
 import { HomeContainer } from '../styles/Home'
 
-export default function Home({countries}){
-  const[keyword,setKeyword] = useState('')
+interface Country {
+  alpha3Code: string;
+  name: string;
+  population: number;
+}
+
+interface HomeProps {
+  countries: Country[];
+}
+
+export default function Home({ countries }: HomeProps) {
+  const [keyword, setKeyword] = useState('')
   const results = countries
 
-  const searcheredCountries = countries.filter(
+  const searchedCountries = results.filter(
     (country) =>
       country.name.toLowerCase().includes(keyword)
   )    
   
-  function onInputChange(e){
+  function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
     e.preventDefault()
     setKeyword(e.target.value.toLowerCase())
   }
   
-  return(
+  return (
     <>
       <Header/>
       <input
         type='text'
         placeholder='Search'
-        onChange={onInputChange}/>
+        onChange={handleInputChange}/>
       <HomeContainer>     
       
       <ul>
-         {searcheredCountries.map((countries) => (
-          <li>
-            <Link href={`/country/${countries.alpha3Code}`}>
+         {searchedCountries.map((country) => (
+          <li key={country.alpha3Code}>
+            <Link href={`/country/${country.alpha3Code}`}>
               <a>
                 <div>
                   <h3>
                     <span>Name</span>
                     <br/>
-                    {countries.name}
+                    {country.name}
                  </h3>
                 <h3>
                  <span>Population</span>
                  <br/>
-                 {countries.population}
+                 {country.population}
                 </h3>
                </div>
               </a>
@@ -55,12 +64,21 @@ export default function Home({countries}){
 }
 
 export const getStaticProps = async () => {
-  const res = await fetch("https://restcountries.com/v2/all");
-  const countries = await res.json();
+  try {
+    const res = await fetch("https://restcountries.com/v2/all");
+    const countries: Country[] = await res.json();
 
-  return {
-    props: {
-      countries,
-    },
-  };
+    return {
+      props: {
+        countries,
+      },
+    };
+  } catch (error) {
+    console.error("Error fetching countries data:", error);
+    return {
+      props: {
+        countries: [],
+      },
+    };
+  }
 };
